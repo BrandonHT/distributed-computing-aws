4a00521936a3f86ab3764d655717259bfbf9ee3e
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-04-11 22:40:25 -0600

    añade explicacion de cada paso

--- limpieza.sh
+++ limpieza.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
+# ------------------ PAS0 1: Cambiar separador , por | excepto para las direcciones
 # Itera sobre cada archivo
 for file in *.csv;
   # Reemplaza todas las comas que no estén dentro de comillas dobles con el carácter '|'
    do csvformat -D '|' "$file" > tmp && mv tmp "$file"; 
    done
 
-# ------------------------------------Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
+# ------------------ PASO 2: Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
 # Crea directorio donde almacenaremos los datos
 mkdir ../clean
 # en cada archivo, filtra los renglones que pertenezcan a registros de la categoria APARATOS ELECTRICOS
 for file in *.csv; do grep -i "APARATOS ELECTRONICOS" "$file" > ../clean/"$file"; done
 
+
+# ----------------- PASO 3: Cambiar formato de coficiacion utf8 a ASCII
 # solo cambia `../clean/` por la ruta donde esten tus datos 
 for file in ../clean/*.csv; do
   iconv -f utf8 -t ascii//TRANSLIT "$file" > "$file.temp"
   mv "$file.temp" "$file"
 done
 
-# une todos los csv en uno solo
+# --------------- PASO 4: une todos los csv en uno solo
 tail -n+1 -q ../clean/*.csv>> electronics_2021.csv
 
 
