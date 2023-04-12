3e86fa1f3d260f26424b3f6ea4c30121f008ac07
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-04-11 19:27:55 -0600

    filtra registros de categoria APARATOS ELECTRICOS

--- limpieza.sh
+++ limpieza.sh
@@ -8,3 +8,10 @@ for file in *.csv;
   # Reemplaza todas las comas que no estén dentro de comillas dobles con el carácter '|'
    do csvformat -D '|' "$file" > tmp && mv tmp "$file"; 
    done
+
+# ------------------------------------Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
+# Crea directorio donde almacenaremos los datos
+mkdir ../clean
+# en cada archivo, filtra los renglones que pertenezcan a registros de la categoria APARATOS ELECTRICOS
+for file in *.csv; do grep -i "APARATOS ELECTRONICOS" "$file" > ../clean/"$file"; done
+
