33e66df5c156e61a054bc6f8145c2c9eb7550582
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-04-11 19:30:53 -0600

    borra linea no necesaria

--- limpieza.sh
+++ limpieza.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Lee el nombre del archivo como argumento
-filename=$1
-
 # Itera sobre cada archivo
 for file in *.csv;
   # Reemplaza todas las comas que no estén dentro de comillas dobles con el carácter '|'
