4547130cd7ecf2147233239074fb8d01f66a96e9
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-04-11 23:12:23 -0600

    ejecuta este programa para limpiar tus datos

--- limpieza.sh
+++ limpieza.sh
@@ -7,7 +7,7 @@ mkdir ../clean
 # Crea archivo donde escribiremos los datos
 touch ../clean/electronics_2021.csv
 # VARIABLE
-PATH_DATACLEAN=$(../clean/electronics_2021.csv)
+PATH_DATACLEAN="../clean/electronics_2021.csv"
 
 # --------------- PASO 1: une todos los csv en uno solo
 tail -n+1 -q *.csv >> $PATH_DATACLEAN
@@ -20,7 +20,7 @@ csvformat -D '|' $PATH_DATACLEAN > tmp && mv tmp "$PATH_DATACLEAN"
 # ------------------ PASO 3.1: Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
 # ----------------- PASO 3.2: Cambiar formato de coficiacion utf8 a ASCII
 
-grep -i "APARATOS ELECTRONICOS" $PATH_DATACLEAN > $PATH_DATACLEAN < iconv -f utf8 -t ascii//TRANSLIT > "$PATH_DATACLEAN.temp"
+grep -i "APARATOS ELECTRONICOS" $PATH_DATACLEAN | iconv -f utf8 -t ascii//TRANSLIT > "$PATH_DATACLEAN.temp"
 mv "$PATH_DATACLEAN.temp" "$PATH_DATACLEAN"
 
 
