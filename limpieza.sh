1ef303e33e52c99090c2e28ae4fc63b3395f02eb
David Damián <110922245+David-Damian@users.noreply.github.com>
2023-04-11 23:02:25 -0600

    simplifica pipeline

--- limpieza.sh
+++ limpieza.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-# ------------------ PAS0 1: Cambiar separador , por | excepto para las direcciones
-# Itera sobre cada archivo
-for file in *.csv;
-  # Reemplaza todas las comas que no estén dentro de comillas dobles con el carácter '|'
-   do csvformat -D '|' "$file" > tmp && mv tmp "$file"; 
-   done
 
-# ------------------ PASO 2: Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
-# Crea directorio donde almacenaremos los datos
+# Para ejecutar este programa, necesitas posicionarte en el directorio donde estan almacenados tus datos
+
+# Crea directorio donde almacenaremos los datos limpios
 mkdir ../clean
-# en cada archivo, filtra los renglones que pertenezcan a registros de la categoria APARATOS ELECTRICOS
-for file in *.csv; do grep -i "APARATOS ELECTRONICOS" "$file" > ../clean/"$file"; done
+# Crea archivo donde escribiremos los datos
+touch ../clean/electronics_2021.csv
+# VARIABLE
+PATH_DATACLEAN=$(../clean/electronics_2021)
+
+# --------------- PASO 1: une todos los csv en uno solo
+tail -n+1 -q *.csv >> $PATH_DATACLEAN
 
 
-# ----------------- PASO 3: Cambiar formato de coficiacion utf8 a ASCII
-# solo cambia `../clean/` por la ruta donde esten tus datos 
-for file in ../clean/*.csv; do
-  iconv -f utf8 -t ascii//TRANSLIT "$file" > "$file.temp"
-  mv "$file.temp" "$file"
-done
+# ------------------ PAS0 2: Cambiar separador , por | excepto para las direcciones
+  # Reemplaza todas las comas que no estén dentro de comillas dobles con el carácter '|'
+csvformat -D '|' $PATH_DATACLEAN > tmp && mv tmp "$PATH_DATACLEAN"
 
-# --------------- PASO 4: une todos los csv en uno solo
-tail -n+1 -q ../clean/*.csv>> electronics_2021.csv
+# ------------------ PASO 3.1: Quedarnos solo con los registros de la categoria APARATOS ELECTRICOS
+# ----------------- PASO 3.2: Cambiar formato de coficiacion utf8 a ASCII
 
+grep -i "APARATOS ELECTRONICOS" $PATH_DATACLEAN > $PATH_DATACLEAN < iconv -f utf8 -t ascii//TRANSLIT > "$PATH_DATACLEAN.temp"
+mv "$file.temp" "$file"
 
 
 
